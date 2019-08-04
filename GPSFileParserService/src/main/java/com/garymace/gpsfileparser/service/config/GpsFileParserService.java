5844b54a79d2a1da051a08c9c8da9f051a85d9ec
GaryMace <gary.mac-elhinney@ucdconnect.ie>
2019-08-04 18:21:28 +0100

    Add base resource with Guice/Dropwizard frameworks running server

--- GPSFileParserService/src/main/java/com/garymace/gpsfileparser/service/config/GpsFileParserService.java
+++ GPSFileParserService/src/main/java/com/garymace/gpsfileparser/service/config/GpsFileParserService.java
@@ -1,19 +1,26 @@
 package com.garymace.gpsfileparser.service.config;
 
-
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 
-public class GpsFileParserService {
-    public static void main(String[] args) throws Exception {
-        buildRestApplication().run(args);
+public class GpsFileParserService extends Application<GpsFileParserConfiguration> {
+    @Override
+    public void initialize(Bootstrap<GpsFileParserConfiguration> bootstrap) {
+        GuiceBundle<GpsFileParserConfiguration> guiceBundle = GuiceBundle.<GpsFileParserConfiguration>newBuilder()
+                .addModule(new GpsFileParserServiceModule())
+                .setConfigClass(GpsFileParserConfiguration.class)
+                .enableAutoConfig(getClass().getPackage().getName())
+                .build();
+        bootstrap.addBundle(guiceBundle);
     }
 
-    private static Application buildRestApplication() {
-        return RestApplication.newBuilder()
-                .modules(
-                        new AutoCloseModule(),
-                        ConfiguredCommonAuthModule.newAppAuth(Scope.INTEGRATIONS_ACCESS),
-                        new GoToWebinarServiceModule())
-                .build();
+    @Override
+    public void run(GpsFileParserConfiguration gpsFileParserConfiguration, Environment environment) throws Exception {
+    }
+
+    public static void main(String[] args) throws Exception {
+        new GpsFileParserService().run(args);
     }
 }
